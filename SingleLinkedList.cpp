3b4ebe489084bc24678dbb59b23ba0382e3be327
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-21 11:26:54 +0700

    menambah void addNode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -19,4 +19,13 @@ public:
         START = NULL;
     }
 
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
   
