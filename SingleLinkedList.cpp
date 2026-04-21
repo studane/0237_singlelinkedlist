dfee4daf6e41c1e764c64a7b3d5c100238d12cd1
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-21 11:45:15 +0700

    menambahkan while di bool listempty

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -69,3 +69,13 @@ public:
     previous = START;
     current = START;
 
+    while (current != NULL && nim != current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    return (current != NULL);
+}
+
+    
